12fa618449f0b1734b94e0ebd8de97e1c01d159f
LarsLinden <lars.linden@outlook.de>
2016-09-16 07:41:50 +0200

    new Font size Clock

--- src/timetracking/RFID_TGL.java
+++ src/timetracking/RFID_TGL.java
@@ -32,7 +32,7 @@ public class RFID_TGL implements TagGainListener {
     public void tagGained(TagGainEvent tagGainEvent)
     {
         clockStop = true;
-        Font font = new Font("DialogInput",Font.PLAIN | Font.BOLD, 5);
+        Font font = new Font("DialogInput",Font.PLAIN | Font.BOLD, 1);
         Font newFont = font.deriveFont(10F);
         jLabelClock.setFont(newFont); 
         jLabelClock.setText("Übertragung läuft...");
