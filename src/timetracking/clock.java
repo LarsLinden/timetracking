16004899cfed83d808f3d343dcccaeef7a956125
LarsLinden <lars.linden@outlook.de>
2016-09-16 07:40:24 +0200

    new Font size Clock

--- src/timetracking/clock.java
+++ src/timetracking/clock.java
@@ -37,10 +37,10 @@ public class clock extends javax.swing.JFrame
                     String time = fmt.format(hour) + ":" + fmt.format(min) + ":" + fmt.format(sec);
 
                     boolean DisplayTimeBool = Timetracking.getClockGo();
+                    if (DisplayTimeBool == false){
                         Font clockFont = new Font("DialogInput",Font.PLAIN | Font.BOLD, 5);
                         Font ClockFont = clockFont.deriveFont(60F);
                         jLabelClock.setFont(ClockFont); 
-                    if (DisplayTimeBool == false){
                         jLabelClock.setText(time);
                     }
                 }
